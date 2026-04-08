937d73821cf7f2c1aabf0f554bfbd7303ba17bb1
kwamedanso <dansokwame96@gmail.com>
2026-04-08 00:23:00 +0000

    sixth commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:5177'; // backend url and port
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || `http://${window.location.hostname}:5177`; // Use env var in prod, fallback to local network in dev
 
 export default axios.create({
     baseURL: BASE_URL,
