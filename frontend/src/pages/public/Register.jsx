ca03f55e73912c417b929478731a6737627be585
kwamedanso <dansokwame96@gmail.com>
2026-04-17 09:40:39 +0000

    Register administrator user

--- frontend/src/pages/public/Register.jsx
+++ frontend/src/pages/public/Register.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from '../../api/axios';
+
 export default function Register() {
+
+let name = "Kwame";
+let email = "kwame@gmail.com";
+let password = "Kwame@123$$";
+let role = "administrator";
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/auth/register', {
+        name,
+        email,
+        password,
+        role
+      });
+      console.log(response.data.message);
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  };
   return (
     <div>
         <h1>Register Page</h1>
         <Link to="/"><button>Home</button></Link>
+        <button onClick={handleRegister}>Register</button>
+        {/* <p>{message}</p> */}
     </div>
   )
 }
